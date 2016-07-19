4d050a900e6ccf185e61f84bcbb371770472fb36
MacRat <m@crat.jp>
2016-07-19 17:09:27 +0900

    Added debug main

---
+++ controller/debug_main.py
@@ -0,0 +1,7 @@
+from communicator import DummyCommunicator
+from controller import KeyboardController
+from window_dummy import DummyWindow
+
+
+if __name__ == '__main__':
+    DummyWindow(KeyboardController(DummyCommunicator)).mainloop()
